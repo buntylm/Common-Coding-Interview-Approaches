1bc1e38573f5d88bc0c1ff430b24d33509678e51
Bunty Madan <buntymadan@Buntys-MacBook-Air.local>
2025-08-13 21:48:14 +0800

    Chapter 1 playground

---
+++ 01-Brute-Force.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+/*
+ Brute force approach
+ - Try all posssible solutions and pick the best or valid one.
+ - Often end up with O(n2) or O(n3)
+ - Not optional solution but
+    - Easy to get correct
+    - Give a reference to compare optimized solution
+ 
+ 
+ General recipe
+ 1. Enumerate all possibilities (loops, recursion, combinations)
+ 2. Check each possibility against the problem's constraints
+ 3. Track the best or first valid solution.
+ */
+
+/*
+ Example 1
+ Problem: Given an array of integers, find the maximum sum of contiguous subarray.
+ 
+ maxSubarraySumBruteForce([-2,1,-3,4,-1,2,1,-5,4]))
+
+ */
+
+
+/*
+ Example 2
+ Problem: Given an array and a target, find two numbers that sum to the target.
+ 
+ twoSumBruteForce([2,7,11,15], 9))
+ */
+
+
