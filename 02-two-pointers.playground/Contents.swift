90f26362b7c8eb7f682ae2bffe33b3d92cf08521
Bunty Madan <mail.bunty.m@gmail.com>
2025-08-16 17:00:51 +0800

    03-two-pointers

--- 02-two-pointers.playground/Contents.swift
+++ 02-two-pointers.playground/Contents.swift
@@ -22,11 +22,25 @@ import Foundation
 */
 
 func twoSumSortedArray(arr: [Int], target: Int) -> [Int] {
-    return []
+    var left = 0
+    var right = arr.count - 1
+    
+    while left < right {
+        let sum = arr[left] + arr[right]
+        
+        if sum == target {
+            return [left, right] // found the pair
+        } else if sum < target {
+            left += 1 // need a bigger sum, move left pointer
+        } else {
+            right -= 1 // need a smaller sum, move right pointer
+        }
+    }
+    
+    return [] // no pair found
 }
 
 // Example
 let arr = [1, 2, 3, 4, 6]
 let target = 6
 print(twoSumSortedArray(arr: arr, target: target)) // [1, 3]
-
